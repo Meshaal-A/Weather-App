a1d10b9446eb6b9d8e5d5bf2917dfcb4b7054818
origin <mishalayubkhan10@gmail.com>
2024-08-31 23:23:24 +0530

    Pollution updated

--- src/app/api/pollution/route.ts
+++ src/app/api/pollution/route.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 import { NextRequest, NextResponse } from "next/server";
 
-export const dynamicParams = true;
-
 export async function GET(req: NextRequest) {
   try {
     const searchParams = req.nextUrl.searchParams;
@@ -10,13 +8,22 @@ export async function GET(req: NextRequest) {
     const lat = searchParams.get("lat");
     const lon = searchParams.get("lon");
 
+    if (!lat || !lon) {
+      return new Response("Missing latitude or longitude", { status: 400 });
+    }
+
     const apiKey = process.env.OPENWEATHERMAP_API_KEY;
 
+    if (!apiKey) {
+      return new Response("API key is missing", { status: 500 });
+    }
+
     const url = `http://api.openweathermap.org/data/2.5/air_pollution?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-    const res = await axios.get(url);
-    return NextResponse.json(res.data);
+    const { data } = await axios.get(url);
+
+    return NextResponse.json(data);
   } catch (error) {
-    console.error("Error in getting pollution data: ", error);
+    console.error("Error in getting pollution data:", error);
     return new Response("Error fetching pollution data", { status: 500 });
   }
 }
